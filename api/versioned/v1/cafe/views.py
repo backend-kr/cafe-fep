dfa8444e1cec47a07e86cbe35ce017031494922a
황광회 <jack7141@nate.com>
2023-06-25 19:15:49 +0900

    [develop] K8S final Test

--- api/versioned/v1/cafe/views.py
+++ api/versioned/v1/cafe/views.py
@@ -16,7 +16,7 @@ class CafeBaseViewSet(MappingViewSetMixin,
 
 class KAKAOCafeViewSet(RetrieveModelMixin, CafeBaseViewSet):
     """
-    retrieve: kakao open api
+    retrieve: kakao open api(K8S를 테스트를 위해 주석 생성해봄)
 
     kakao open api 카페 리스트 반환
     """
